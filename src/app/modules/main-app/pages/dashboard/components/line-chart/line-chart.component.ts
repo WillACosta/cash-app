f9f354586621f36787e41e4e7e95ac76612f163a
willACosta <williamamaral521@gmail.com>
2021-12-14 15:49:29 -0300

    refactor: add material warn pallete colors and create chart service

--- src/app/modules/main-app/pages/dashboard/components/line-chart/line-chart.component.ts
+++ src/app/modules/main-app/pages/dashboard/components/line-chart/line-chart.component.ts
@@ -4,18 +4,16 @@ import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { Actions, ofActionSuccessful, Select } from '@ngxs/store';
 import { combineLatest, Observable, Subscription } from 'rxjs';
-import { format } from 'date-fns';
 
 import { MainState } from '../../../../../../modules/main-app/store/state/main.state';
 import { Transaction } from '../../../../../../models/transaction.model';
 import { UpdateTransactions } from '../../../../../../shared/store/shared.actions';
+import { ChartService } from 'src/app/services/chart.service';
 
 import {
   lineChartConfiguration,
   lineChartOptions,
 } from './chart-configuration';
-
-import * as _ from 'lodash';
 
 @Component({
   selector: 'app-line-chart',
@@ -23,7 +21,7 @@ import * as _ from 'lodash';
   styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent implements OnInit, OnDestroy {
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private chartService: ChartService) {}
 
   public lineChartData: ChartConfiguration['data'] = lineChartConfiguration;
   public lineChartOptions: ChartConfiguration['options'] = lineChartOptions;
@@ -54,8 +52,10 @@ export class LineChartComponent implements OnInit, OnDestroy {
         this.incomingTransactions$,
         this.expenseTransactions$,
       ]).subscribe(([incoming, expense]) => {
-        const incomingResults = this.getGroupedAmountAndMonthLabels(incoming);
-        const expenseResults = this.getGroupedAmountAndMonthLabels(expense);
+        const incomingResults =
+          this.chartService.getGroupedAmountAndMonthLabels(incoming);
+        const expenseResults =
+          this.chartService.getGroupedAmountAndMonthLabels(expense);
 
         if (
           incomingResults.amountValues.length &&
@@ -63,10 +63,11 @@ export class LineChartComponent implements OnInit, OnDestroy {
           incomingResults.amountValues.length <
             expenseResults.amountValues.length
         ) {
-          this.lineChartData.datasets[0].data = this.getCorrelatedMonthValues(
-            incomingResults,
-            expenseResults
-          );
+          this.lineChartData.datasets[0].data =
+            this.chartService.getCorrelatedMonthValues(
+              incomingResults,
+              expenseResults
+            );
         } else {
           this.lineChartData.datasets[0].data = incomingResults.amountValues;
         }
@@ -77,15 +78,16 @@ export class LineChartComponent implements OnInit, OnDestroy {
           incomingResults.amountValues.length >
             expenseResults.amountValues.length
         ) {
-          this.lineChartData.datasets[1].data = this.getCorrelatedMonthValues(
-            incomingResults,
-            expenseResults
-          );
+          this.lineChartData.datasets[1].data =
+            this.chartService.getCorrelatedMonthValues(
+              incomingResults,
+              expenseResults
+            );
         } else {
           this.lineChartData.datasets[1].data = expenseResults.amountValues;
         }
 
-        this.lineChartData.labels = this.getUniqueMonthLabels(
+        this.lineChartData.labels = this.chartService.getUniqueMonthLabels(
           incomingResults.labelValues,
           expenseResults.labelValues
         );
@@ -101,102 +103,5 @@ export class LineChartComponent implements OnInit, OnDestroy {
           this.chart?.update();
         })
     );
-  }
-
-  getGroupedAmountAndMonthLabels(data: Array<any>) {
-    const orderedByDate = this.sortOrder(
-      data.map((item) => {
-        return {
-          ...item,
-          date: new Date(item.date),
-        };
-      })
-    );
-
-    const groupedByMont = _.groupBy(orderedByDate, (item) =>
-      format(new Date(item.date), 'MMMM')
-    );
-
-    const convertedArray = this.objectToArray(groupedByMont);
-    const monthKeys = Object.keys(groupedByMont);
-
-    return this.getResultObject(convertedArray, monthKeys);
-  }
-
-  private sortOrder(data: Array<any>) {
-    return data.sort((a, b) => {
-      if (a.date < b.date) {
-        return -1;
-      }
-
-      if (a.date > b.date) {
-        return 1;
-      }
-
-      return 0;
-    });
-  }
-
-  private objectToArray(obj: any) {
-    return Object.keys(obj).map((key) => obj[key]);
-  }
-
-  private getResultObject(array: Array<any>, monthKeys: Array<string>) {
-    let result: Array<any> = [];
-
-    array.forEach((item) => {
-      const totalAmount = item.reduce(
-        (acc: any, curr: any) => acc + curr.amount,
-        0
-      );
-
-      result.push(totalAmount);
-    });
-
-    return { amountValues: result, labelValues: monthKeys };
-  }
-
-  private getUniqueMonthLabels(
-    incomingLabels: Array<any>,
-    expenseLabels: Array<any>
-  ): Array<string> {
-    const mergedLabels = _.merge(incomingLabels, expenseLabels);
-    return _.uniq(mergedLabels);
-  }
-
-  private getCorrelatedMonthValues(incoming: any, expense: any) {
-    if (!incoming.amountValues?.length && !expense.amountValues?.length)
-      return [];
-
-    if (!incoming.amountValues?.length) return [];
-    if (!expense.amountValues?.length) return [];
-
-    let result: Array<any> = [];
-
-    if (incoming.amountValues.length > expense.amountValues.length) {
-      incoming.labelValues.forEach((label: string, index: number) => {
-        if (expense.labelValues.includes(label)) {
-          result.push(
-            expense.amountValues[_.indexOf(expense.labelValues, label)]
-          );
-        } else {
-          result.push(0);
-        }
-      });
-    }
-
-    if (expense.amountValues.length > incoming.amountValues.length) {
-      expense.labelValues.forEach((label: string, index: number) => {
-        if (incoming.labelValues.includes(label)) {
-          result.push(
-            incoming.amountValues[_.indexOf(incoming.labelValues, label)]
-          );
-        } else {
-          result.push(0);
-        }
-      });
-    }
-
-    return result;
   }
 }
---
+++ src/app/services/chart.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import { format } from 'date-fns';
+
+import * as _ from 'lodash';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChartService {
+  constructor() {}
+
+  getGroupedAmountAndMonthLabels(data: Array<any>) {
+    const orderedByDate = this.sortOrder(
+      data.map((item) => {
+        return {
+          ...item,
+          date: new Date(item.date),
+        };
+      })
+    );
+
+    const groupedByMont = _.groupBy(orderedByDate, (item) =>
+      format(new Date(item.date), 'MMMM')
+    );
+
+    const convertedArray = this.objectToArray(groupedByMont);
+    const monthKeys = Object.keys(groupedByMont);
+
+    return this.getResultObject(convertedArray, monthKeys);
+  }
+
+  getUniqueMonthLabels(
+    incomingLabels: Array<any>,
+    expenseLabels: Array<any>
+  ): Array<string> {
+    const mergedLabels = _.merge(incomingLabels, expenseLabels);
+    return _.uniq(mergedLabels);
+  }
+
+  getCorrelatedMonthValues(incoming: any, expense: any) {
+    if (!incoming.amountValues?.length && !expense.amountValues?.length)
+      return [];
+
+    if (!incoming.amountValues?.length) return [];
+    if (!expense.amountValues?.length) return [];
+
+    let result: Array<any> = [];
+
+    if (incoming.amountValues.length > expense.amountValues.length) {
+      incoming.labelValues.forEach((label: string, index: number) => {
+        if (expense.labelValues.includes(label)) {
+          result.push(
+            expense.amountValues[_.indexOf(expense.labelValues, label)]
+          );
+        } else {
+          result.push(0);
+        }
+      });
+    }
+
+    if (expense.amountValues.length > incoming.amountValues.length) {
+      expense.labelValues.forEach((label: string, index: number) => {
+        if (incoming.labelValues.includes(label)) {
+          result.push(
+            incoming.amountValues[_.indexOf(incoming.labelValues, label)]
+          );
+        } else {
+          result.push(0);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  private sortOrder(data: Array<any>) {
+    return data.sort((a, b) => {
+      if (a.date < b.date) {
+        return -1;
+      }
+
+      if (a.date > b.date) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  private objectToArray(obj: any) {
+    return Object.keys(obj).map((key) => obj[key]);
+  }
+
+  private getResultObject(array: Array<any>, monthKeys: Array<string>) {
+    let result: Array<any> = [];
+
+    array.forEach((item) => {
+      const totalAmount = item.reduce(
+        (acc: any, curr: any) => acc + curr.amount,
+        0
+      );
+
+      result.push(totalAmount);
+    });
+
+    return { amountValues: result, labelValues: monthKeys };
+  }
+}
