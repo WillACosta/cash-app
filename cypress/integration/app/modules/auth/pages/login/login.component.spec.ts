7a6ae0f8ce9483f90ce5ae148eb8c06aa1ca2a0a
William <williamamaral521@gmail.com>
2022-02-09 16:19:27 -0300

    test(login.component.spec): add test and style code

--- cypress/integration/app/modules/auth/pages/login/login.component.spec.ts
+++ cypress/integration/app/modules/auth/pages/login/login.component.spec.ts
@@ -1,20 +1,22 @@
 describe('Login Component', () => {
-  const userEmail = 'eve.holt@reqres.in';
-  const userPassword = '1234';
+  const userEmail = 'eve.holt@reqres.in'
+  const userPassword = '1234'
+  const baseUrl = `${Cypress.config().baseUrl}`
 
   it('should redirect to login page if user is not logged in', () => {
-
-  });
+   cy.visit(baseUrl)
+   cy.url().should('eq', `${baseUrl}/auth/login`)
+  })
 
   it('should fill the form to sign the user and redirect to dashboard page', () => {
     cy.login(userEmail, userPassword)
-    cy.url().should('eq', `${Cypress.config().baseUrl}/dashboard`);
-  });
+    cy.url().should('eq', `${baseUrl}/dashboard`)
+  })
 
   it('should display error message when user credentials are invalid', () => {
-    const userEmail = 'any.user@reqres.in';
+    const userEmail = 'any.user@reqres.in'
     cy.login(userEmail, userPassword)
 
-    cy.get('.toast-message').contains('Não foi possível');
-  });
-});
+    cy.get('.toast-message').contains('Não foi possível')
+  })
+})
